1ab47b3720cca4d052e9bce8a128775f1bf1d156
osamikoyo <v.pylayev@mail.ru>
2025-08-23 07:39:41 +0500

    update storage

--- go.mod
+++ go.mod
@@ -5,10 +5,13 @@ go 1.25.0
 require (
 	github.com/bytedance/sonic v1.14.0 // indirect
 	github.com/bytedance/sonic/loader v0.3.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.5 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/redis/go-redis/v9 v9.12.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
--- storage/storage.go
+++ storage/storage.go
@@ -50,3 +50,101 @@ func (s *Storage) CreateNewChunk(chunk *models.Chunk) error {
 
 	return nil
 }
+
+func (s *Storage) GetChunk(project string) (*models.Chunk, error) {
+	var chunk models.Chunk
+
+	res := s.db.Where(&models.Chunk{
+		InUse:   true,
+		Project: project,
+	}).First(&chunk)
+	if err := res.Error; err != nil {
+		s.logger.Error("failed fetch chunk",
+			zap.String("project", project),
+			zap.Error(err))
+
+		return nil, fmt.Errorf("failed fetch chunk: %v", err)
+	}
+
+	return &chunk, nil
+}
+
+func (s *Storage) ListVersions(project string) ([]int, error) {
+	var versions []models.Chunk
+
+	res := s.db.Where(&models.Chunk{
+		Project: project,
+	}).Find(&versions)
+	if err := res.Error; err != nil {
+		s.logger.Error("failed find chunks",
+			zap.String("project", project),
+			zap.Error(err))
+
+		return nil, fmt.Errorf("failed find chunks: %v", err)
+	}
+
+	resp := make([]int, len(versions))
+	for i, v := range versions {
+		resp[i] = v.Version
+	}
+
+	s.logger.Info("fetched versions",
+		zap.String("project", project))
+
+	return resp, nil
+}
+
+func (s *Storage) RollChunkOn(project string, version int) error {
+	res := s.db.Where(&models.Chunk{
+		Project: project,
+		InUse:   true,
+	}).Update("in_use", false)
+	if err := res.Error; err != nil {
+		s.logger.Error("failed to update old version",
+			zap.String("project", project),
+			zap.Int("version", version),
+			zap.Error(err))
+
+		return fmt.Errorf("failed to update old version: %v", err)
+	}
+
+	res = s.db.Where(&models.Chunk{
+		Project: project,
+		Version: version,
+	}).Update("in_use", true)
+	if err := res.Error; err != nil {
+		s.logger.Error("failed roll chunk on",
+			zap.String("project", project),
+			zap.Int("version", version),
+			zap.Error(err))
+
+		return fmt.Errorf("failed to update new version: %v", err)
+	}
+
+	s.logger.Info("successfully roll chunk on",
+		zap.String("project", project),
+		zap.Int("version", version))
+
+	return nil
+}
+
+func (s *Storage) DeleteConfig(project string, version int) error {
+	res := s.db.Where(&models.Chunk{
+		Project: project,
+		Version: version,
+	}).Delete(&models.Chunk{})
+	if err := res.Error; err != nil {
+		s.logger.Error("failed delete chunk",
+			zap.String("project", project),
+			zap.Int("version", version),
+			zap.Error(err))
+
+		return fmt.Errorf("failed delete config: %v", err)
+	}
+
+	s.logger.Info("chunk deleted successfully",
+		zap.String("project", project),
+		zap.Int("version", version))
+
+	return nil
+}
