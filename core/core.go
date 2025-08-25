0164c6400d946cd1b7f8cc41ae0d6dee7d5f0da0
osamikoyo <v.pylayev@mail.ru>
2025-08-25 10:47:12 +0500

    update core

---
+++ core/core.go
@@ -0,0 +1,145 @@
+package core
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/osamikoyo/yoconf/casher"
+	"github.com/osamikoyo/yoconf/logger"
+	"github.com/osamikoyo/yoconf/models"
+	"github.com/osamikoyo/yoconf/retrier"
+	"github.com/osamikoyo/yoconf/storage"
+	"go.uber.org/zap"
+)
+
+const RetrierCount = 5
+
+var ErrNilInput = errors.New("nil input")
+
+type Core struct {
+	casher  *casher.Casher
+	storage *storage.Storage
+	logger  *logger.Logger
+
+	timeout time.Duration
+}
+
+func NewCore(
+	casher *casher.Casher,
+	storage *storage.Storage,
+	logger *logger.Logger,
+	timeout time.Duration,
+) *Core {
+	return &Core{
+		casher:  casher,
+		storage: storage,
+		logger:  logger,
+		timeout: timeout,
+	}
+}
+
+func (c *Core) Close() error {
+	return c.casher.Close()
+}
+
+func (c *Core) context() (context.Context, context.CancelFunc) {
+	return context.WithTimeout(context.Background(), c.timeout)
+}
+
+func (c *Core) NewConfig(chunk *models.Chunk) error {
+	if chunk == nil {
+		return ErrNilInput
+	}
+
+	ctx, cancel := c.context()
+	defer cancel()
+
+	err := retrier.Try(RetrierCount, func() error {
+		return c.storage.CreateNewChunk(ctx, chunk)
+	})
+	if err != nil {
+		c.logger.Error("failed create chunk", zap.Error(err))
+
+		return err
+	}
+
+	err = retrier.Try(RetrierCount, func() error {
+		return c.casher.CreateChunk(ctx, chunk)
+	})
+	if err != nil {
+		c.logger.Error("failed create chunk in cash", zap.Error(err))
+
+		return err
+	}
+
+	return nil
+}
+
+func (c *Core) RollOn(project string, version int) error {
+	if project == "" || version < 1 {
+		return ErrNilInput
+	}
+
+	ctx, cancel := c.context()
+	defer cancel()
+
+	err := retrier.Try(RetrierCount, func() error {
+		return c.storage.RollChunkOn(ctx, project, version)
+	})
+	if err != nil {
+		c.logger.Error("failed roll chunk on", zap.Error(err))
+
+		return err
+	}
+
+	data, err := c.casher.GetData(ctx, project)
+	if err != nil {
+		c.logger.Error("failed roll chunk on", zap.Error(err))
+	}
+
+	err = retrier.Try(RetrierCount, func() error {
+		return c.casher.DeleteChunk(ctx, project)
+	})
+	if err != nil {
+		c.logger.Error("failed roll chunk on", zap.Error(err))
+
+		return err
+	}
+
+	err = retrier.Try(RetrierCount, func() error {
+		return c.storage.CreateNewChunk(ctx, &models.Chunk{
+			Data:    data,
+			Version: version,
+			Project: project,
+			InUse:   false,
+		})
+	})
+	if err != nil {
+		c.logger.Error("failed roll chunk on", zap.Error(err))
+
+		return err
+	}
+
+	return nil
+}
+
+func (c *Core) GetConfig(project string) (string, error) {
+	ctx, cancel := c.context()
+	defer cancel()
+
+	data, err := c.casher.GetData(ctx, project)
+	if err == nil {
+		c.logger.Info("successfully fetched config", zap.String("data", data))
+
+		return data, nil
+	}
+
+	chunk, err := c.storage.GetChunk(ctx, project)
+	if err != nil {
+		c.logger.Error("failed get config", zap.Error(err))
+	}
+
+	c.logger.Info("successfully fetched config", zap.Any("chunk", chunk))
+	return chunk.Data, nil
+}
